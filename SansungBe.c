ba4832272bd344bb18f9859119b71aee53a1f6a9
[kong] <wlsrn3684@naver.com>
2019-12-04 22:22:27 -0800

    sansungbee.c

--- SansungBe.c
+++ SansungBe.c
@@ -27,9 +27,9 @@ int itoa(int n, char *str);
 void findWord(char *str);
 node *makeNode();
 void makePlusOne();
-void addQueue(const char *str, int col);
+void addSQueue(const char *str, int col);
 const char *returnWord();
-void draw(int row, int col, const char *str);
+void Sandraw(int row, int col, const char *str);
 void startGame();
 
 int hp = 100;
@@ -42,9 +42,8 @@ char hpText[3] = {0};
 node *ptr = 0;
 char enterText[20] = {0};
 int enterHere = 0;
-int sleep_time = 2;
+int thread_sleep_time = 2;
 pthread_t t1;
-
 void function(int signum) {
     reset();     // Release memory
     curs_set(1); // cursor activation
@@ -81,6 +80,34 @@ void reset() {
 }
 
 // Put in queue
+<<<<<<< HEAD
+void addSQueue(const char *str, int col) {
+  node *temp = 0;
+  node *temp2 = 0;
+
+  if (ptr == 0) {
+    ptr = makeNode();
+    strcpy(ptr->str, str);
+    ptr->row = 1;
+    ptr->col = col;
+  }
+  else {
+    temp = makeNode();
+    strcpy(temp->str, str);
+    temp->row = 1;
+    temp->col = col;
+    temp->link = ptr;
+    ptr = temp;
+    makePlusOne();
+  }
+
+  length++;
+
+  if (length > 15) {
+    while (temp->link) {
+      temp2 = temp;
+      temp = temp->link;
+=======
 void addQueue(const char *str, int col) {
     node *temp = 0;
     node *temp2 = 0;
@@ -98,6 +125,7 @@ void addQueue(const char *str, int col) {
         temp->link = ptr;
         ptr = temp;
         makePlusOne();
+>>>>>>> 1962d5e3f2cf2efe2a580ce5eecaf99636e4e628
     }
 
     length++;
@@ -126,6 +154,15 @@ void addQueue(const char *str, int col) {
 }
 
 // Show word on screen
+<<<<<<< HEAD
+void* thread_1(void *none) {
+  int t = thread_sleep_time ;
+
+  while (hp > 0) {
+    node *temp = 0;
+    addSQueue(returnWord(), (rand() % 40) + 8);
+    temp = ptr;
+=======
 void *thread_1(void *none) {
     int t = sleep_time;
 
@@ -133,12 +170,19 @@ void *thread_1(void *none) {
         node *temp = 0;
         addQueue(returnWord(), (rand() % 40) + 8);
         temp = ptr;
+>>>>>>> 1962d5e3f2cf2efe2a580ce5eecaf99636e4e628
 
         while (temp) {
 
+<<<<<<< HEAD
+			Sandraw(temp->row, temp->col, temp->str);
+      temp = temp->link;
+    }
+=======
             draw(temp->row, temp->col, temp->str);
             temp = temp->link;
         }
+>>>>>>> 1962d5e3f2cf2efe2a580ce5eecaf99636e4e628
 
         move(17, 12);
 
@@ -164,6 +208,52 @@ int itoa(int n, char *str) {
 
 // finding input string and deleting in queue
 void findWord(char *str) {
+<<<<<<< HEAD
+  node *temp = 0;
+  temp = ptr;
+
+  while (temp) {
+    if (!strcmp(temp->str, str)) {
+			score += strlen(temp->str);
+			strcpy(temp->str, "");
+			if (score > 50 && thread_sleep_time  != 1) {
+				thread_sleep_time  = 1;
+				pthread_cancel(t1);
+				pthread_create(&t1, NULL, &thread_1, NULL);
+			}
+			if (score > 100) {
+				pthread_cancel(t1);
+				Sandraw(1 , 0, "                                                             ");
+				Sandraw(2 , 0, "                    clear                                    ");
+				Sandraw(3 , 0, "                                                             ");
+				Sandraw(4 , 0, "                           clear                             ");
+				Sandraw(5 , 0, "                                                             ");
+				Sandraw(6 , 0, "                                                             ");
+				Sandraw(7 , 0, "           clear                                             ");
+				Sandraw(8 , 0, "                                          clear              ");
+				Sandraw(9 , 0, "                                                             ");
+				Sandraw(10, 0, "                                                             ");
+				Sandraw(11, 0, "                      clear               clear              ");
+				Sandraw(12, 0, "                                                             ");
+				Sandraw(13, 0, "                                                             ");
+				Sandraw(14, 0, "                                                             ");
+				Sandraw(15, 0, "                                                             ");
+				Sandraw(16, 0, "   ----------------------------------------------------------");
+				Sandraw(17, 0, "   | Enter :                     | Score :      | HP :      |");
+				itoa(score, scoreText);
+				move(17, 43);
+				addstr("      ");
+				move(17, 43);
+				addstr(scoreText);
+			  itoa(hp, hpText);
+			  move(17, 55);
+			  addstr("     ");
+			  move(17, 55);
+			  addstr(hpText);
+				move(17, 12);
+			}
+      return;
+=======
     node *temp = 0;
     temp = ptr;
 
@@ -245,6 +335,7 @@ void findWord(char *str) {
             return;
         } else
             temp = temp->link;
+>>>>>>> 1962d5e3f2cf2efe2a580ce5eecaf99636e4e628
     }
 }
 
@@ -278,12 +369,21 @@ const char *returnWord() {
 }
 
 //
+<<<<<<< HEAD
+void Sandraw(int row, int col,const char *str) {
+  move(row, 0);
+  addstr("                                                            ");
+  move(row, col);
+  addstr(str);
+  refresh();
+=======
 void draw(int row, int col, const char *str) {
     move(row, 0);
     addstr("                                                            ");
     move(row, col);
     addstr(str);
     refresh();
+>>>>>>> 1962d5e3f2cf2efe2a580ce5eecaf99636e4e628
 }
 
 void startGame() {
@@ -312,6 +412,65 @@ void startGame() {
     move(17, 55);
     addstr(hpText);
 
+<<<<<<< HEAD
+  clear();
+	Sandraw(0 , 0, "   ----------------------------------------------------------");
+  Sandraw(16, 0, "   ----------------------------------------------------------");
+  Sandraw(17, 0, "   | Enter :                     | Score :      | HP :      |");
+  Sandraw(18, 0, "   ----------------------------------------------------------");
+
+	itoa(score, scoreText);
+	move(17, 43);
+	addstr("      ");
+	move(17, 43);
+	addstr(scoreText);
+  itoa(hp, hpText);
+  move(17, 55);
+  addstr("     ");
+  move(17, 55);
+  addstr(hpText);
+
+  pthread_create(&t1, NULL, &thread_1, NULL);
+
+  while (hp > 0) {
+    for (enterHere = 0; enterHere < 20;) {
+      int c = getch();
+
+      if (c == '\n') {
+        enterText[enterHere] = '\0';
+        findWord(enterText);
+
+        for (i = 0; i < 20; i++) {
+          enterText[i] = '\0';
+        }
+
+        Sandraw(17, 0, "   | Enter :                     | Score :      | HP :      |");
+				itoa(score, scoreText);
+				move(17, 43);
+				addstr("      ");
+				move(17, 43);
+				addstr(scoreText);
+			  itoa(hp, hpText);
+			  move(17, 55);
+			  addstr("     ");
+			  move(17, 55);
+			  addstr(hpText);
+				move(17, 12);
+
+        break;
+      }
+      else if (c == 127) {
+        if (enterHere > 0) {
+          enterText[--enterHere] = '\0';
+          move(17, 12);
+          addstr("                     ");
+          move(17, 12);
+          addstr(enterText);
+        }
+        else {
+          move(17, 12);
+          addstr("                     ");
+=======
     pthread_create(&t1, NULL, &thread_1, NULL);
 
     while (hp > 0) {
@@ -360,6 +519,7 @@ void startGame() {
             }
 
             refresh();
+>>>>>>> 1962d5e3f2cf2efe2a580ce5eecaf99636e4e628
         }
     }
     pthread_join(t1, NULL);
